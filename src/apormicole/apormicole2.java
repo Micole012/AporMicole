b1dbba0ae5b8f054ccb56a37914c794f2d5d8256
Micole012 <apormicole1215@gmail.com>
2025-07-25 15:19:28 -0700

    First push

---
+++ src/apormicole/apormicole2.java
@@ -0,0 +1,13 @@
+
+
+package apormicole;
+
+
+public class apormicole2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Activity 2");
+    }
+    
+}
